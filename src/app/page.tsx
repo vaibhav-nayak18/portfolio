0a55dc2574d8db72e4ec715f7954df3ceefd342a
vaibhav nayak <vaibhavnayak018@gmail.com>
2023-11-04 23:39:29 +0530

    mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
+import { Background, Navbar } from '@/components';
+
 export default function Home() {
   return (
     <div className="w-full h-full  absolute">
-      <video
-        autoPlay
-        muted
-        loop
-        className=" object-cover h-full w-full "
-      >
-        <source
-          src="/blackhole.webm"
-          type="video/webm"
-        />
-      </video>
-      <div className="text-white absolute overflow-y-auto top-0 bottom-0 left-0 right-0"></div>
+      <Background />
+      <div className="text-white absolute overflow-y-auto top-0 bottom-0 left-0 right-0">
+        {/* <Navbar /> */}
+      </div>
     </div>
   );
 }
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+import Navbar from './navbar';
+import Background from './backgroud';
+export { Navbar, Background };
