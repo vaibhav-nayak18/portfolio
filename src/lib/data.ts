6b408f7111928b60b44f5359c563bad315da6d51
vaibhav <vaibhavnayak018@gmail.com>
2024-01-05 23:59:32 +0530

    data added

---
+++ src/lib/data.ts
@@ -0,0 +1,37 @@
+export const links = [
+  {
+    name: "Home",
+    hash: "home",
+  },
+  {
+    name: "About",
+    hash: "about",
+  },
+  {
+    name: "Projects",
+    hash: "projects",
+  },
+  {
+    name: "Skill",
+    hash: "skill",
+  },
+  {
+    name: "Contact",
+    hash: "contact",
+  },
+] as const;
+
+export const programminLang = ["Javascript", "Typescript", "Node Js", "Go"];
+
+export const frameWork = [
+  "React Js",
+  "React Native",
+  "Next Js",
+  "Tailwind Css",
+  "Express Js",
+  "Fiber - (Go backend frame work)",
+] as const;
+
+export const database = ["Mongodb"];
+
+export const otherSkill = ["HTML", "CSS", "Git", "Neovim", "VS Code"] as const;
